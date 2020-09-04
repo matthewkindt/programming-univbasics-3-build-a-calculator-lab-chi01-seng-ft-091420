1cef5a8a66eca2fabdc1d4cbc14a2e01271c639b
Matthew Kindt <matthewkindt@yahoo.com>
2020-09-04 03:31:27 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,28 @@
-def addition(num1, num2)
-
+def addition(num1 = 5, num2 = 4)
+addition = 5 + 4
 end
+puts addition
 
-def subtraction(num1, num2)
-
+def subtraction(num1 = 10, num2 = 5)
+  subtraction = 10 - 5
 end
+puts subtraction
 
-def division(num1, num2)
-
+def division(num1 = 50, num2 = 2)
+  division = 50 / 2
 end
+puts division
 
-def multiplication(num1, num2)
-
+def multiplication(num1 = 4, num2 = 30)
+  multiplication = 4 * 30
 end
+puts multiplication
 
-def modulo(num1, num2)
-
+def modulo(num1 = 34, num2 = 5)
+  modulo = 34 % 5
 end
+puts modulo
 
-def square_root(num)
-
+def square_root(num = 81)
+puts square_root.sqrt(81)
 end
